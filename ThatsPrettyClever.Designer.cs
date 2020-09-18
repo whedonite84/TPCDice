1712be0e0b2065c10ff71ffc5df027e7ed0d050e
DESKTOP-V8KU2I7\thewi <blackneyevan@gmail.com>
2020-09-17 21:38:21 -0400

    initial commit to master

---
+++ ThatsPrettyClever.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace TPCDice
+{
+    partial class ThatsPrettyClever
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ThatsPrettyClever));
+            this.txtYellow = new System.Windows.Forms.TextBox();
+            this.txtBlue = new System.Windows.Forms.TextBox();
+            this.txtGreen = new System.Windows.Forms.TextBox();
+            this.txtOrange = new System.Windows.Forms.TextBox();
+            this.txtPurple = new System.Windows.Forms.TextBox();
+            this.txtWhite = new System.Windows.Forms.TextBox();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblRollNum = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtYellow
+            // 
+            this.txtYellow.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtYellow.BackColor = System.Drawing.Color.Yellow;
+            this.txtYellow.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtYellow.Location = new System.Drawing.Point(102, 22);
+            this.txtYellow.Name = "txtYellow";
+            this.txtYellow.ReadOnly = true;
+            this.txtYellow.Size = new System.Drawing.Size(83, 83);
+            this.txtYellow.TabIndex = 0;
+            this.txtYellow.Text = "6";
+            this.txtYellow.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtYellow.Click += new System.EventHandler(this.txtDie_Click);
+            // 
+            // txtBlue
+            // 
+            this.txtBlue.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtBlue.BackColor = System.Drawing.Color.Blue;
+            this.txtBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBlue.Location = new System.Drawing.Point(191, 22);
+            this.txtBlue.Name = "txtBlue";
+            this.txtBlue.ReadOnly = true;
+            this.txtBlue.Size = new System.Drawing.Size(83, 83);
+            this.txtBlue.TabIndex = 1;
+            this.txtBlue.Text = "6";
+            this.txtBlue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBlue.Click += new System.EventHandler(this.txtDie_Click);
+            // 
+            // txtGreen
+            // 
+            this.txtGreen.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtGreen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.txtGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGreen.Location = new System.Drawing.Point(280, 22);
+            this.txtGreen.Name = "txtGreen";
+            this.txtGreen.ReadOnly = true;
+            this.txtGreen.Size = new System.Drawing.Size(83, 83);
+            this.txtGreen.TabIndex = 2;
+            this.txtGreen.Text = "6";
+            this.txtGreen.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtGreen.Click += new System.EventHandler(this.txtDie_Click);
+            // 
+            // txtOrange
+            // 
+            this.txtOrange.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtOrange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtOrange.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOrange.Location = new System.Drawing.Point(102, 111);
+            this.txtOrange.Name = "txtOrange";
+            this.txtOrange.ReadOnly = true;
+            this.txtOrange.Size = new System.Drawing.Size(83, 83);
+            this.txtOrange.TabIndex = 3;
+            this.txtOrange.Text = "6";
+            this.txtOrange.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtOrange.Click += new System.EventHandler(this.txtDie_Click);
+            // 
+            // txtPurple
+            // 
+            this.txtPurple.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtPurple.BackColor = System.Drawing.Color.Purple;
+            this.txtPurple.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPurple.Location = new System.Drawing.Point(191, 111);
+            this.txtPurple.Name = "txtPurple";
+            this.txtPurple.ReadOnly = true;
+            this.txtPurple.Size = new System.Drawing.Size(83, 83);
+            this.txtPurple.TabIndex = 4;
+            this.txtPurple.Text = "6";
+            this.txtPurple.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPurple.Click += new System.EventHandler(this.txtDie_Click);
+            // 
+            // txtWhite
+            // 
+            this.txtWhite.AllowDrop = true;
+            this.txtWhite.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtWhite.BackColor = System.Drawing.Color.White;
+            this.txtWhite.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWhite.Location = new System.Drawing.Point(280, 111);
+            this.txtWhite.Name = "txtWhite";
+            this.txtWhite.ReadOnly = true;
+            this.txtWhite.Size = new System.Drawing.Size(83, 83);
+            this.txtWhite.TabIndex = 5;
+            this.txtWhite.Text = "6";
+            this.txtWhite.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtWhite.Click += new System.EventHandler(this.txtDie_Click);
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnRoll.Location = new System.Drawing.Point(376, 200);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(75, 23);
+            this.btnRoll.TabIndex = 8;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnReset.Location = new System.Drawing.Point(295, 200);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::TPCDice.Properties.Resources.Platter;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 229);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(442, 247);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblRollNum
+            // 
+            this.lblRollNum.AutoSize = true;
+            this.lblRollNum.Location = new System.Drawing.Point(99, 6);
+            this.lblRollNum.Name = "lblRollNum";
+            this.lblRollNum.Size = new System.Drawing.Size(63, 13);
+            this.lblRollNum.TabIndex = 9;
+            this.lblRollNum.Text = "Roll #X of 3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(369, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Capture";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ThatsPrettyClever
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(463, 469);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblRollNum);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnRoll);
+            this.Controls.Add(this.txtWhite);
+            this.Controls.Add(this.txtPurple);
+            this.Controls.Add(this.txtOrange);
+            this.Controls.Add(this.txtGreen);
+            this.Controls.Add(this.txtBlue);
+            this.Controls.Add(this.txtYellow);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ThatsPrettyClever";
+            this.Text = "That\'s Pretty Clever!";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox txtYellow;
+        public System.Windows.Forms.TextBox txtBlue;
+        public System.Windows.Forms.TextBox txtGreen;
+        public System.Windows.Forms.TextBox txtOrange;
+        public System.Windows.Forms.TextBox txtPurple;
+        public System.Windows.Forms.TextBox txtWhite;
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblRollNum;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ThatsPrettyClever.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Threading;
+using System.Windows.Forms;
+using TPCDice.Properties;
+
+namespace TPCDice
+{
+    public partial class ThatsPrettyClever : Form
+    {
+        private DiceCollection _dice;
+        private readonly Random _rng = new Random();
+        private Die _mostRecentlyHeld;
+
+        private int _rollNum;
+        private DiceCollection _diceCap;
+
+        private int RollNum
+        {
+            get => _rollNum;
+            set
+            {
+                _rollNum = Math.Min(value, 3);
+                lblRollNum.Text = $"Roll #{RollNum} of 3";
+            }
+        }
+
+        public ThatsPrettyClever()
+        {
+            InitializeComponent();
+
+            txtYellow.BackColor = Color.Yellow;
+            txtBlue.BackColor = Color.CornflowerBlue;
+            txtGreen.BackColor = Color.Green;
+            txtOrange.BackColor = Color.Orange;
+            txtPurple.BackColor = Color.DarkOrchid;
+            txtWhite.BackColor = Color.White;
+
+            _dice = DiceCollection.Load(this, _rng);
+
+            RollNum = 1;
+        }
+
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            if (_mostRecentlyHeld != null && _dice.HoldCount >= RollNum) _dice.Place(_mostRecentlyHeld);
+
+            RollNum = _dice.HoldCount + 1;
+
+            Refresh();
+            if (btnRoll.Text == "Roll") _dice.Roll();
+        }
+
+        private void txtDie_Click(object sender, EventArgs e)
+        {
+            var die = _dice[(TextBox)sender];
+
+            if (!die.Held && RollNum <= _dice.HoldCount) return;
+
+            die.Held = !die.Held;
+            if (die.Held) _mostRecentlyHeld = die;
+
+            btnRoll.Text = _dice.HoldCount == 3 ? "Score" : "Roll";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            _dice.Reset();
+            _mostRecentlyHeld = null;
+            RollNum = 1;
+            btnRoll.Text = "Roll";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Capture")
+            {
+                _diceCap = _dice;
+                button1.Text = "Restore";
+            }
+            else
+            {
+                _dice = _diceCap;
+                button1.Text = "Capture";
+            }
+        }
+    }
+
+    public class Die
+    {
+        public TextBox Txt;
+        public int Value => int.Parse(Txt.Text);
+
+        public bool Held
+        {
+            get => Txt.ForeColor == Color.Red;
+            set
+            {
+                if (Held == value) return;
+                Txt.ForeColor = value ? Color.Red : Color.Black;
+                Txt.Font = new Font(Txt.Font, value ? FontStyle.Strikeout : FontStyle.Regular);
+            }
+        }
+
+        private readonly Random _rng;
+        private int _platterOffset = 230;
+
+        public bool OnPlatter
+        {
+            get => Txt.Location.Y >= _platterOffset;
+            set
+            {
+                if (OnPlatter == value) return;
+                int offset = value ? _platterOffset : -_platterOffset;
+                Txt.Location = new Point(Txt.Location.X, Txt.Location.Y + offset);
+            }
+        }
+
+        public Die(TextBox txt, Random rng)
+        {
+            Txt = txt;
+            _rng = rng;
+        }
+
+        public void Roll()
+        {
+            Txt.Text = _rng.Next(1, 7).ToString();
+            Txt.Refresh();
+        }
+
+        public void MoveToPlatter()
+        {
+            Txt.Location = new Point(Txt.Location.X, Txt.Location.Y + 207);
+        }
+
+        public void Reset()
+        {
+            Held = OnPlatter = false;
+        }
+    }
+
+    public class DiceCollection : List<Die>
+    {
+        public Die Yellow;
+        public Die Blue;
+        public Die Green;
+        public Die Orange;
+        public Die Purple;
+        public Die White;
+
+        public DiceCollection(ThatsPrettyClever form, Random rng)
+        {
+            Yellow = new Die(form.txtYellow, rng);
+            Blue = new Die(form.txtBlue, rng);
+            Green = new Die(form.txtGreen, rng);
+            Orange = new Die(form.txtOrange, rng);
+            Purple = new Die(form.txtPurple, rng);
+            White = new Die(form.txtWhite, rng);
+        }
+
+        public int HoldCount => this.Count(d => d.Held);
+
+        public static DiceCollection Load(ThatsPrettyClever form, Random rng)
+        {
+            var retVal = new DiceCollection(form, rng);
+            retVal.AddRange(new List<Die> {retVal.Yellow, retVal.Blue, retVal.Green, retVal.Orange, retVal.Purple, retVal.White});
+            return retVal;
+        }
+
+        public Die this[TextBox key] => Find(d => d.Txt == key);
+
+        public void Roll()
+        {
+            new SoundPlayer(Resources.RollSound).Play();
+
+            for (int i = 0; i < 150; i++)
+            {
+                foreach (var d in this.Where(d => !d.Held && !d.OnPlatter)) d.Roll();
+                Thread.Sleep(10);
+            }
+        }
+
+        public void Reset()
+        {
+            foreach (var d in this) d.Reset();
+        }
+
+        public void Place(Die die)
+        {
+            int upperBound = this.Count(d => d.Held) < 3 ? die.Value : 7;
+            foreach (var d in this.Where(d => !d.Held && d.Value < upperBound)) d.OnPlatter = true;
+        }
+    }
+}
